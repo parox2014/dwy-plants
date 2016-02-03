11b5c9abe6c1b86c8f09552b1d9178fbd66f4927
William Du <William Du>
2016-02-03 20:41:05 +0800

    update

--- www/js/services.js
+++ www/js/services.js
@@ -1,75 +1,74 @@
-(function(){
+(function () {
   angular
-    .module('app.services', [])
-    .factory('Config',function(){
+    .module('app.services', ['ngResource'])
+    .factory('Config', function () {
       return {
-        api:{
-          SIGNIN:'',
-          SIGNOUT:'',
-          SIGNUP:'/signup',
-          PROFILE:'/profile'
+        api: {
+          SIGNIN: '',
+          SIGNOUT: '',
+          SIGNUP: '/signup',
+          PROFILE: '/profile'
         },
-        events:{
-          REQUEST_START:'$requestStart',
-          REQUEST_ERROR:'$requestError',
-          RESPONSE_ERROR:'$responseError',
-          RESPONSE:'$response'
+        events: {
+          REQUEST_START: '$requestStart',
+          REQUEST_ERROR: '$requestError',
+          RESPONSE_ERROR: '$responseError',
+          RESPONSE: '$response'
         }
       }
     })
-    .factory('Session', [function(){
+    .factory('Session', [function () {
       return {
-        user:null,
-        destroy:function(){
-          this.user=null
+        user: null,
+        destroy: function () {
+          this.user = null
         }
       }
     }])
     //登录注册服务
-    .factory('Sign',SignService)
-
+    .factory('Sign', SignService)
+    .factory('Schedule',ScheduleService)
     //地区服务
-    .factory('Region',RegionService)
+    .factory('Region', RegionService)
     //请求拦截器
-    .factory('Interceptor',Interceptor)
-    .config(function($locationProvider,$httpProvider){
+    .factory('Interceptor', Interceptor)
+    .config(function ($locationProvider, $httpProvider) {
       $locationProvider.hashPrefix('!');
       //设置ajax请求拦截器
       $httpProvider.interceptors.push('Interceptor');
       //配置请求头
-      $httpProvider.defaults.headers.common['X-Requested-With']='XMLHttpRequest';
+      $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     });
 
 
-
-  function SignService($http,Session,$q,Config){
-    var api=Config.api;
+  function SignService($http, Session, $q, Config) {
+    var api = Config.api;
 
     return {
-      signin:signin,
-      signout:signout,
-      signup:signup,
-      updateProfile:updateProfile
+      signin: signin,
+      signout: signout,
+      signup: signup,
+      updateProfile: updateProfile
     };
 
-    function signin(){
+    function signin() {
 
     }
 
-    function signout(){
+    function signout() {
 
     }
 
-    function signup(account){
-      var deffered=$q.defer();
+    function signup(account) {
+      var deffered = $q.defer();
 
-      $http.post(api.SIGNUP,account)
-        .success(function(resp){
+      $http.post(api.SIGNUP, account)
+        .success(function (resp) {
 
-          Session.user=resp;
+          Session.user = resp;
           deffered.resolve(resp);
         })
-        .error(function(err){
+        .error(function (err) {
 
           deffered.reject(err);
         });
@@ -77,15 +76,15 @@
       return deffered.promise;
     }
 
-    function updateProfile(profile){
+    function updateProfile(profile) {
 
-      var deffered=$q.defer();
+      var deffered = $q.defer();
 
-      $http.post(api.PROFILE,profile)
-        .success(function(resp){
+      $http.post(api.PROFILE, profile)
+        .success(function (resp) {
           deffered.resolve(resp);
         })
-        .error(function(err){
+        .error(function (err) {
 
           deffered.reject(err);
         });
@@ -94,45 +93,53 @@
     }
   }
 
-  function RegionService($http){
+  function RegionService($http) {
     return {
-      queryCountryList:getCountry,
-      queryProviceList:getProvice,
-      queryCityList:getCity
+      queryCountryList: getCountry,
+      queryProviceList: getProvice,
+      queryCityList: getCity
     };
 
-    function getCountry(){
+    function getCountry() {
 
     }
 
-    function getProvice(){
+    function getProvice() {
 
     }
 
-    function getCity(){
+    function getCity() {
 
     }
   }
 
-  function ScheduleService
+  function ScheduleService(Config,$http,$q,$resource) {
+    return $resource(Config.api.SIGNIN+'/:id',{
+      id:'@id'
+    },{
+      update:{
+        method:'PUT'
+      }
+    })
+  }
 
-  function Interceptor($q,$rootScope,Config){
-    var events=Config.events;
+  function Interceptor($q, $rootScope, Config) {
+    var events = Config.events;
     return {
-      request:function(config){
-        $rootScope.$broadcast(events.REQUEST_START,config);
+      request: function (config) {
+        $rootScope.$broadcast(events.REQUEST_START, config);
         return config;
       },
-      requestError:function(rejection){
-        $rootScope.$broadcast(events.REQUEST_ERROR,rejection);
+      requestError: function (rejection) {
+        $rootScope.$broadcast(events.REQUEST_ERROR, rejection);
         return $q.reject(rejection);
       },
-      response:function(response){
-        $rootScope.$broadcast(events.RESPONSE,response);
+      response: function (response) {
+        $rootScope.$broadcast(events.RESPONSE, response);
         return response;
       },
-      responseError: function(rejection) {
-        $rootScope.$broadcast(events.RESPONSE_ERROR,rejection);
+      responseError: function (rejection) {
+        $rootScope.$broadcast(events.RESPONSE_ERROR, rejection);
         return $q.reject(rejection);
       }
     };
